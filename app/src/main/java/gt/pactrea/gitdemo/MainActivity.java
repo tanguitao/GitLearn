1ce40f09c9e40ad16e00d2e5cffdff983711d4c5
P0156902 <guitao.tan@pactera.com>
2020-05-20 10:38:47 +0800

    develop添加注释

--- app/src/main/java/gt/pactrea/gitdemo/MainActivity.java
+++ app/src/main/java/gt/pactrea/gitdemo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*这是develop添加的注释*/
     }
 }
